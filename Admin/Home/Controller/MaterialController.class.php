bf25ce835c0e363ead07195a658143ef1dc793b9
gaojingming <gaojingm@hotmail.com>
2015-07-19 10:03:06 +0800

    添加ckfinder插件

---
+++ Admin/Home/Controller/MaterialController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Home\Controller;
+use Common\Controller\BackendController;
+
+class MaterialController extends BackendController {
+
+	public function index() {
+		$this->display();
+	}
+
+}
