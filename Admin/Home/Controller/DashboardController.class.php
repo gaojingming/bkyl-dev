dd31a3a2e0a6c7e47d7289844e095263765a405d
gaojingming <gaojingm@hotmail.com>
2015-08-21 22:48:33 +0800

    完成后台每日统计功能

--- Admin/Home/Controller/DashboardController.class.php
+++ Admin/Home/Controller/DashboardController.class.php
@@ -8,6 +8,37 @@ class DashboardController extends BackendController {
 	 * 后台首页
 	 */
 	public function index() {
+		$sta_db = M('statistic');
+		$sta_rs = $sta_db->order('date')->select();
+
+		$tot_view = 0;
+		$mon_view = 0;
+		$mon_stat = array();
+		$yes_view = 0;
+		$today_view = 0;
+
+		foreach ($sta_rs as $v) {
+			$tot_view += $v['view_num'];
+			if ($v['date'] >= date('Y-m-d', strtotime('-30 day'))) {
+				$mon_stat[] = array(
+					'date' => $v['date'],
+					'view' => $v['view_num']
+				);
+				$mon_view += $v['view_num'];
+
+				if ($v['date'] == date('Y-m-d', strtotime('-1 day')))
+					$yes_view = $v['view_num'];
+
+				if ($v['date'] == date('Y-m-d'))
+					$today_view = $v['view_num'];
+			}
+		}
+
+		$this->assign('tot_view', $tot_view);
+		$this->assign('mon_view', $mon_view);
+		$this->assign('yes_view', $yes_view);
+		$this->assign('today_view', $today_view);
+		$this->assign('mon_stat', $mon_stat);
 		$this->display();
 	}
 
